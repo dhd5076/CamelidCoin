24ea4d2c59dc2eea330beec2045fc0e952462a0d
Dylan Dunn <dylandunn17@icloud.com>
2023-04-27 00:36:25 -0400

    added node schema

---
+++ src/models/node.js
@@ -0,0 +1,28 @@
+/**
+ * @class Compute Node
+ */
+import { Schema, model, models} from 'mongoose';
+
+const nodeSchema =  new Schema({
+    /**
+     * The owner of the node
+     */
+    owner: {
+        type: Schema.Types.ObjectId, ref: 'User',
+        required: [true, "Node must have owner"]
+    },
+    /**
+     * List of models that the node is capable of computing
+     */
+    capabilities: {
+        type: [{
+            type: Schema.Types.ObjectId, 
+            ref: 'Model'
+        }],
+        required: [true, "Node must have capabilities listed"]
+    }
+});
+
+const Node = models.Node || model('Node', nodeSchema)
+
+export default Node;
