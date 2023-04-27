1e8c85e7d1c8871189266cd6c18e7f3396d160ca
Dylan Dunn <dylandunn17@icloud.com>
2023-04-27 01:15:14 -0400

    Added model schema

---
+++ src/models/model.js
@@ -0,0 +1,23 @@
+/**
+ * @class Model
+ */
+import { Schema, model, models} from 'mongoose';
+
+const modelSchema =  new Schema({
+    /**
+     * The name of the model
+     */
+    name: {
+        type: String,
+        required: [true, "Model must have name"]
+    },
+    parameters: {
+        type: Object,
+        required: false
+    },
+
+});
+
+const Model = models.Node || model('Model', modelSchema)
+
+export default Model;
