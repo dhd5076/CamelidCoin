9eabe835a2376e73a9fa109d0c358bb3e78b556f
Dylan Dunn <dylandunn17@icloud.com>
2023-05-01 16:45:51 -0400

    mispelled fike

---
+++ src/transaction.js
@@ -0,0 +1,41 @@
+/**
+ * @module Transcation used for creating and verifying transcations
+ */
+
+import { KeyPair } from './utils/cryptoUtils';
+
+/**
+ * @class Transcation
+ */
+export class Transcation {
+    constructor(fromAddress, toAddress, amount, fee) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.fee = fee;
+        this.amount = amount;
+        this.identified = 
+    }
+
+    /**
+     * Signs a transcation with a given keypair
+     * @param {KeyPair} keyPair 
+     */
+    sign(keyPair) {
+        const data = JSON.stringify(this);
+        const signature = keyPair.sign(data);
+        this.signature = signature;
+    }   
+
+    verify(publicKey) {
+        return new Promise((resolve, reject) => {
+            const data = JSON.stringify(this);
+            KeyPair.verifySignature(publicKey, data)
+            .then((isVerified) => {
+                resolve(isVerified)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+        })
+    }
+}
