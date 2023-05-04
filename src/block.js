99a49d175b2e5e67cd7e2fedfd6f0735cfd715de
Dylan Dunn <dylandunn17@icloud.com>
2023-05-04 03:04:48 -0400

    Implemented some block methods

--- src/block.js
+++ src/block.js
@@ -2,24 +2,59 @@
  * @module Block used for handling blocks
  */
 
+import {Transaction} from './transaction';
+
 class Block {
     constructor(transcations, previousHash) {
+        //Merkle root, when brain isnt tired
         this.timestamp = new Date();
         this.transactions = transactions;
         this.previousHash = previousHash;
+        this.hash = "";
         this.nonce = 0;
     }
 
+    /**
+     * Initialize the block, not done with contructor because init is not instant.
+     */
     init() {
-        this.hash = calculateHash();
+        return new Promise((resolve, reject) => {
+            this.hash = calculateHash();
+            resolve();
+        })
     }
 
+    /**
+     * Calculate the hash of the block
+     * @returns {Promise.<String} the hash of the block
+     */
     calculateHash() {
+        return new Promise((resolve, reject) => {
+            const hash = SHA256(
+                this.index +
+                  this.previousHash +
+                  this.timestamp +
+                  JSON.stringify(this.transactions)
+              ).toString();
+              resolve(hash);
+        })
+      }
 
-    }
-
-    hasTranscation() {
-
+    /**
+     * Verify whether or not the block has this transcation
+     * @param {Transaction} transaction 
+     */
+    hasTransaction(transaction) {
+        return new Promise((resolve, reject) => {
+            for (let i = 0; i < this.transactions.length; i++) {
+                if (
+                  this.transactions[i].hash == transaction.hash
+                ) {
+                  resolve(true);
+                }
+              }
+              resolve(false)
+        })
     }
 
     validate() {
