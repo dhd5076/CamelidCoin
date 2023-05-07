a2f425f1db61c9642ba6c73e220afc9dbbd3b311
Dylan Dunn <dylandunn17@icloud.com>
2023-05-07 02:36:03 -0400

    added logger, added transaction type

--- src/blockchain/transaction.js
+++ src/blockchain/transaction.js
@@ -11,17 +11,42 @@ export class Transcation {
     constructor(inputs, outputs, timestamp) {
         this.inputs = inputs;
         this.outputs = outputs;
+        this.type = 
         this.timestamp = timestamp;
         this.hash = this.calculateHash();
         this.signature = null;
     }
 
     /**
-     * Create a new transcation
-     * @param {Wallet} wallet wallet object to use to send from and sign the transaction
-     * @param {*} output 
+     * Enum representing the different types of transactions
+     * @readonly 
+     * @enum {Number}
      */
-    static create(wallet, ) {
+    static get Type() {
+        return { 
+            /**
+             * Transaction type: wallet to wallet transfer.
+             * Cannot be invalidated if valid while in pool
+             */
+            WALLET_TO_WALLET: 1,
+            /**
+             * Transaction type: payment by client to compute node
+             * Can be invalidated by INVALID_OUTPUT or NO_OUTPUT
+             */
+            PAYMENT_FOR_JOB: 2,
+            /**
+             * Transaction: 
+             */
+            JOB_VOUCH: 3
+        }
+    }
+
+    /**
+     * Create a new WALLET_TO_WALLET transaction
+     * @param {Wallet} wallet wallet object to use to send from and sign the transaction
+     * @param {String} output wallet address of the destination of the transaction
+     */
+    static create_W2W_Transaction(wallet, ) {
         return new Promise((resolve, reject) => {
             return new Transcation(
                 [],
@@ -36,8 +61,13 @@ export class Transcation {
      */
     calcaluteHash() {
         return new Promise((resolve, reject) => {
-            const data = JSON.stringify(this);
-            const hash = crypto.createHash('sha256').update(data).digest('hex');
+            try {
+                const data = JSON.stringify(this);
+                const hash = crypto.createHash('sha256').update(data).digest('hex');
+                resolve(hash)
+            } catch(error) {
+                reject(error)
+            }
         })
     }
 
