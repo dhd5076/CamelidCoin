a3045a26ce0535f817afbdb1f6b167a147f6d702
Dylan Dunn <dylandunn17@icloud.com>
2023-05-22 22:22:10 -0400

    Fixed race conditions in for loops

--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -3,6 +3,7 @@
  * @author Dylan Dunn
  */
 
+import { getHash } from '../utils/cryptoUtils.js';
 import { Transaction } from './transaction.js';
 
 /**
@@ -36,35 +37,68 @@ export class Block {
      */
     getMerkleProof(id) {
         return new Promise((resolve, reject) => {
-          const transactions = this.transactions;
-      
-          const targetTransaction = transactions.find(transaction => transaction.hash === hash);
-      
-          if (!targetTransaction) {
-            reject(new Error('Transaction not found'));
-          }
-      
+            const transactions = this.transactions;
+        
+            const targetTransaction = transactions.find(transaction => transaction.hash === hash);
+        
+            if (!targetTransaction) {
+                reject(new Error('Transaction not found'));
+            }
+        
             const merkleProof = [];
-    
+
             const targetIndex = transactions.findIndex(transaction => transaction.hash === hash);
-    
+
             let currentIndex = targetIndex;
             let currentHash = targetTransaction.hash;
-    
+
             while (currentIndex >= 0) {
-            const siblingIndex = currentIndex % 2 === 0 ? currentIndex + 1 : currentIndex - 1;
-            const siblingHash = siblingIndex < transactions.length ? transactions[siblingIndex].hash : currentHash;
-    
-            const concatenatedHash = siblingIndex < currentIndex ? siblingHash + currentHash : currentHash + siblingHash;
-            const hashedValue = SHA256(concatenatedHash).toString();
-    
-            merkleProof.push(siblingHash);
-    
-            currentHash = hashedValue;
-            currentIndex = Math.floor(currentIndex / 2);
+                const siblingIndex = currentIndex % 2 === 0 ? currentIndex + 1 : currentIndex - 1;
+                const siblingHash = siblingIndex < transactions.length ? transactions[siblingIndex].hash : currentHash;
+
+                const concatenatedHash = siblingIndex < currentIndex ? siblingHash + currentHash : currentHash + siblingHash;
+
+                getHash(concatenatedHash).then(hash => {
+                    hashedValue = hash;
+                }).then(() => {
+                    merkleProof.push(siblingHash);
+
+                    currentHash = hashedValue;
+                    currentIndex = Math.floor(currentIndex / 2);
+                });
             }
-    
+
             resolve(merkleProof);
+        });
+    }
+
+    /**
+     * @method verifyMerkleProof
+     * @description Verifies the validity of a Merkle proof for a given transaction
+     * @param {String} hash the hash of the transaction
+     * @param {String} merkleRoot the Merkle root of the block
+     * @param {Array} merkleProof the Merkle proof for the transaction
+     * @returns {Boolean} true if the Merkle proof is valid, false otherwise
+     */
+    verifyMerkleProof(hash, merkleRoot, merkleProof) {
+        return new Promise(async (resolve, reject) => {
+            const targetTransactionHash = this.transactions.find(transaction => transaction.hash === hash)?.hash;
+        
+            if (!targetTransactionHash) {
+                reject(new Error('Transaction not found'));
+            }
+        
+            let computedHash = targetTransactionHash;
+        
+            for (const siblingHash of merkleProof) {
+                const concatenatedHash = computedHash < siblingHash ? computedHash + siblingHash : siblingHash + computedHash;
+                await getHash(concatenatedHash)
+                    .then(hashedValue => {
+                        computedHash = hashedValue;
+                    });
+            }
+        
+            resolve(computedHash === merkleRoot);
         });
     }
 
@@ -74,7 +108,7 @@ export class Block {
      * @returns {Promise.<String, Error>} the merkle root of the block
      */
     calculateMerkleRoot() {
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
             const transactions = this.transactions;
 
             if(transactions.length === 0) {
@@ -89,8 +123,11 @@ export class Block {
                 for(let i = 0; i < merkleTree.length; i += 2) {
                     const left = merkleTree[i];
                     const right = merkleTree[i + 1] || left; //In case of odd number of hashes
-                    const concatHash = SHA256(left + right).toString();
-                    nextLevel.push(concatHash);
+
+                    //TODO make sure this stops race condition with for loop
+                    await getHash(left + right).then(hash => {
+                        nextLevel.push(hash);
+                    });
                 }
 
                 merkleTree = nextLevel;
